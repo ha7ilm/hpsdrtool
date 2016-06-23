6ca372491f3feb7f0ea50a71040afaf77d2c030b
ha7ilm <retzlerandras@gmail.com>
2016-06-23 09:14:57 +0200

    Added GPL license

--- hpsdrtool.py
+++ hpsdrtool.py
@@ -1,3 +1,21 @@
+"""
+This file is part of Hpsdrtool.
+Copyright (c) 2016 by Andras Retzler <randras@sdr.hu>
+
+Hpsdrtool is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Hpsdrtool is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Hpsdrtool.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 #!/usr/bin/python2
 
 from socket import *
